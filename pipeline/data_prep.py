758f7fd7b9392854ffc1921d75ce0e820d2ba33a
Alexander J. Thompson <bildbiderise@gmx.net>
2023-01-01 00:00:00 -0500

    Initial professional architecture setup

---
+++ pipeline/data_prep.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def prepare_dataset(file_path, target_col):
+    df = pd.read_csv(file_path)
+    X = df.drop(target_col, axis=1)
+    y = df[target_col]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled, y_train, y_test
