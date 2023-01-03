96cefdcdef6899e99fc0ddeaf6bf07cfc94a5efd
Alexander J. Thompson <bildbiderise@gmx.net>
2023-01-03 14:09:00 -0500

    Optimization and performance tuning

--- pipeline/data_prep.py
+++ pipeline/data_prep.py
@@ -12,4 +12,4 @@ def prepare_dataset(file_path, target_col):
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
     return X_train_scaled, X_test_scaled, y_train, y_test
-\n# Maintenance log 1\n# Maintenance log 3\n# Maintenance log 4\n# Maintenance log 5\n# Maintenance log 6
+\n# Maintenance log 1\n# Maintenance log 3\n# Maintenance log 4\n# Maintenance log 5\n# Maintenance log 6\n# Maintenance log 8
